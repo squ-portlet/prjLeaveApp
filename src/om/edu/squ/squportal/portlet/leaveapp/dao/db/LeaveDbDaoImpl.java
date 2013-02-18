a198b6d71afe46deb0d7ab98029c90d16bd534da
Bhabesh Mukhopadhyay <bhabesh@squ.edu.om>
2013-02-18 10:56:15 +0000

    git-svn-id: http://svnsrv.squ.edu.om:9880/portal_dev/trunk/prjLeaveApp@148 c54ebf58-d1e1-4e32-95a7-835a07b49a07

--- src/om/edu/squ/squportal/portlet/leaveapp/dao/db/LeaveDbDaoImpl.java
+++ src/om/edu/squ/squportal/portlet/leaveapp/dao/db/LeaveDbDaoImpl.java
@@ -25,7 +25,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *
- * 
+ * .
  */
 package om.edu.squ.squportal.portlet.leaveapp.dao.db;
 
